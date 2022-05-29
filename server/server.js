81c60587aefcc7ae923243d6477659dbddf768a9
Justin Kim <jk07661@gmail.com>
2022-05-29 07:51:27 -0400

    worked on express api routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 require("dotenv").config();
+const morgan = require("morgan");
 const express = require("express");
-
 const app = express();
+
+// Setting up some middleware
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("yeah our middleware");
+  next();
+});
 
 //Get all restaurants
 
@@ -17,6 +26,28 @@ app.get("/api/v1/restaurants", (req, res) => {
 app.get("/api/v1/restaurants/:id", (req, res) => {
   console.log(req.params.id);
   res.send("hi");
+});
+
+// Create a Restaurant
+
+app.post("/api/v1/restaurants", (req, res) => {
+  console.log(req.body);
+});
+
+// Update a Restaurant
+
+app.put("/api/v1/restaurants/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  res.send("updated restaurant");
+});
+
+//Delete a Restaurant
+
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+  res.status(204).json({
+    status: "success",
+  });
 });
 
 const port = process.env.PORT || 3001;
